999f14bb2d646f7072798065abfd652cbe8d6477
Jonathan Carroll <jonathaniscarroll@gmail.com>
2017-07-27 01:11:48 -0400

    neural network test

---
+++ postCapPropUnity/Assets/neuralNetwork.cs
@@ -0,0 +1,29 @@
+﻿
+
+public class neuralNetwork{
+
+	private int[] layers;
+	private float[][] neurons;
+	private float [][][] weights;
+
+	public NeuralNetwork(int[] layers)
+	{
+		this.layers = new int[layers.Length];
+		for (int i = 0;i<layers.Length;i++)
+		{
+			this.layers[i] = layers[i];
+		}
+
+		InitNeurons();
+		InitWeights();
+	}
+
+	private void InitNeurons()
+	{
+
+	}
+	private void InitWeights(){
+
+	}
+
+}
